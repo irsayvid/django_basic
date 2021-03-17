3017e2ca0c9950995c224deb925a9d2aaba54cfb
irsayvid <dsdarimisetti@gmail.com>
2021-03-17 17:27:57 +0530

    database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 
-posts = [
-    {
-        'author': 'DivyaSD',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 09, 2020'
-    },
-    {
-        'author': 'IrsAyvid',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 10, 2020'
-    }
-]
+# posts = [
+#     {
+#         'author': 'DivyaSD',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'March 09, 2020'
+#     },
+#     {
+#         'author': 'IrsAyvid',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'March 10, 2020'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
